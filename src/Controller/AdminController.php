3e98b8f019cc8ae0c924e2f6681011e360199241
Ilqar Huseynli <h.ilqar@prospect.local>
2019-06-13 16:21:49 +0400

    first commit

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/admin", name="admin_home")
+     */
+    public function index()
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+}
